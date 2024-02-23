3b370b24ceb501f7aa6f3b60be2062faef1676a7
Tyuiu.MyasnikovaES1@outlook.com <etichoniva12@gmail.com>
2024-02-23 17:56:18 +0500

    Task5

---
+++ Tyuiu.MyasnikovaES.Sprint1.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MyasnikovaES.Sprint1.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MyasnikovaES.Sprint1.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fe3e9e3f-edf6-4f15-81e6-983c621e1629")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
